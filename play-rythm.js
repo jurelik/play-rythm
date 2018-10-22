732cf24e1a30b3aed361e99621fde161d3a91ed1
jurelik <jure@likar.me>
2018-10-22 22:26:57 +0100

    done playing for the day

--- play-rythm.js
+++ play-rythm.js
@@ -1,1 +1,22 @@
-console.log('sup');
+let context = new (window.AudioContext || window.webkitAudioContext)();
+let buffer;
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'sounds/kick.wav', true);
+xhr.responseType = 'arraybuffer';
+
+xhr.onload = function() {
+  buffer = xhr.response;
+}
+xhr.send();
+
+function play() {
+  let source = context.createBufferSource();
+  source.connect(context.destination);
+  console.log(buffer);
+  context.decodeAudioData(buffer.slice(), function(decoded) {
+    source.buffer = decoded;
+  });
+  console.log(buffer);
+  source.start();
+}
