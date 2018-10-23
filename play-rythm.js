01c81125d1ac4e9200d407ed9a805e04282cf86e
jurelik <jure@likar.me>
2018-10-23 22:08:31 +0100

    rewrote playing sound into one reusable function

--- play-rythm.js
+++ play-rythm.js
@@ -1,22 +1,31 @@
 let context = new (window.AudioContext || window.webkitAudioContext)();
-let buffer;
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'sounds/kick.wav', true);
-xhr.responseType = 'arraybuffer';
-
-xhr.onload = function() {
-  buffer = xhr.response;
-}
-xhr.send();
-
-function play() {
+function playSound(url) {
+  let xhr = new XMLHttpRequest();
+  let buffer;
   let source = context.createBufferSource();
   source.connect(context.destination);
-  console.log(buffer);
-  context.decodeAudioData(buffer.slice(), function(decoded) {
+  xhr.open('GET', url, true);
+  xhr.responseType = 'arraybuffer';
+  
+  xhr.onload = function() {
+    buffer = xhr.response;
+    context.decodeAudioData(buffer, function(decoded) {
+      source.buffer = decoded;
+    });
+    source.start();
+  }
+  xhr.send();
+  // return buffer;
+}
+
+function play(sound) {
+  let source = context.createBufferSource();
+  source.connect(context.destination);
+  // console.log(buffer);
+  context.decodeAudioData(sound.slice(), function(decoded) {
     source.buffer = decoded;
   });
-  console.log(buffer);
+  // console.log(buffer);
   source.start();
 }
