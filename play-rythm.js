c1b63db3d8733fdc2db92337e7479d8d8bc91e1c
jurelik <jure@likar.me>
2018-10-24 22:15:21 +0100

    loading and playing sound combined into a function

--- play-rythm.js
+++ play-rythm.js
@@ -1,25 +1,32 @@
 let context = new (window.AudioContext || window.webkitAudioContext)();
 
-function loadSound(url, sound) {
+let playSound = function(url) {
   let xhr = new XMLHttpRequest();
-  let buffer;
+  let sound = context.createBufferSource();
+  sound.connect(context.destination);
   xhr.open('GET', url, true);
   xhr.responseType = 'arraybuffer';
-  
   xhr.onload = function() {
-    sound = xhr.response;
+    context.decodeAudioData(xhr.response, decoded => {
+      sound.buffer = decoded;
+      sound.start();
+    });
   }
   xhr.send();
-  // return buffer;
 }
 
-function play(sound) {
-  let source = context.createBufferSource();
-  source.connect(context.destination);
-  // console.log(buffer);
-  context.decodeAudioData(sound.slice(), function(decoded) {
-    source.buffer = decoded;
-  });
-  // console.log(buffer);
-  source.start();
-}
+document.addEventListener('keypress', e => {
+  if (e.key == 'a') {
+    playSound('sounds/kick.wav');
+  }
+});
+document.addEventListener('keypress', e => {
+  if (e.key == 'w') {
+    playSound('sounds/snare.wav');
+  }
+});
+document.addEventListener('keypress', e => {
+  if (e.key == 'e') {
+    playSound('sounds/hat.wav');
+  }
+});
